e76157978d340aaed0d526036defb4e3a40adf99
Lilia Sapurina <yoyxoxo@gmail.com>
2015-08-15 01:14:53 +0300

    Уменьшила задержку и открыла комментарии

--- fixture/session.py
+++ fixture/session.py
@@ -37,9 +37,9 @@ class SessionHelper:
 
     def ensure_login(self, username, password):
         wd = self.app.wd
-       # if self.is_logged_in():
-       #     if self.is_logged_in_as(username):
-       #         return
-       #     else:
-       #         self.logout()
+        if self.is_logged_in():
+           if self.is_logged_in_as(username):
+               return
+           else:
+               self.logout()
         self.login(username, password)
