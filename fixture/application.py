be72e06929de8f02bc882beb166954ccaf33ecb2
Lilia Sapurina <yoyxoxo@gmail.com>
2015-08-29 16:35:57 +0300

    Реализованы опции командной строки
    Для типа браузера, стенда, логина и пароля

--- fixture/application.py
+++ fixture/application.py
@@ -1,21 +1,29 @@
 __author__ = '1'
 
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.address import AddressHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, baseUrl):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.address = AddressHelper(self)
+        self.baseUrl = baseUrl
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.baseUrl)
 
     def destroy(self):
         self.wd.quit()
