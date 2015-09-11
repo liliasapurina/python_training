17f060c6aea67cb494aa40e3591780e4cc2553ba
Lilia Sapurina <yoyxoxo@gmail.com>
2015-09-11 23:05:33 +0300

    Добавлены тесты на удаление групп и контактов с загрузкой данных из БД

--- fixture/address.py
+++ fixture/address.py
@@ -47,11 +47,26 @@ class AddressHelper:
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
 
+    def select_address_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
     def delete_address_by_index(self, index):
         wd = self.app.wd
         # open groups page
         self.open_address_page()
         self.select_address_by_index(index)
+        # submit deletion
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
+        self.open_address_page()
+        self.address_cache = None
+
+    def delete_address_by_id(self, id):
+        wd = self.app.wd
+        # open groups page
+        self.open_address_page()
+        self.select_address_by_id(id)
         # submit deletion
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
@@ -157,8 +172,4 @@ class AddressHelper:
         wd = self.app.wd
         self.open_address_view_by_index(index)
         text = wd.find_element_by_id("content").text
-        #phone = re.search("H: (.*)",text).group(1)
-        #mobilephone = re.search("M: (.*)",text).group(1)
-        #workphone = re.search("W: (.*)",text).group(1)
-        #secondaryphone = re.search("F: (.*)",text).group(1)
         return Address(all_fields=text)
--- test/test_del_address.py
+++ test/test_del_address.py
@@ -1,14 +1,14 @@
 __author__ = '1'
 from model.address import Address
-from random import randrange
+import random
 
-def test_delete_first_address(app):
-    if app.address.count() == 0:
+def test_delete_first_address(app, db):
+    if len(db.get_address_list()) == 0:
         app.address.create(Address(name="Maria",middlename="Olegovna",lastname="Vasnecova",nickname="OOMa",company="Pepsi",phone="345-55-66"))
-    old_addresses = app.address.get_address_list()
-    index = randrange(len(old_addresses))
-    app.address.delete_address_by_index(index)
+    old_addresses = db.get_address_list()
+    address = random.choice(old_addresses)
+    app.address.delete_address_by_id(address.id)
     assert len(old_addresses) - 1 == app.address.count()
-    new_addresses = app.address.get_address_list()
-    old_addresses[index:index + 1] = []
+    new_addresses = db.get_address_list()
+    old_addresses.remove(address)
     assert old_addresses == new_addresses
