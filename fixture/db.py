345d480d9f2901519c88df5594ce701ef8362a3c
Lilia Sapurina <yoyxoxo@gmail.com>
2015-09-11 22:28:03 +0300

    Проверка равенства данных из веб и БД для контактов

--- fixture/db.py
+++ fixture/db.py
@@ -1,6 +1,7 @@
 __author__ = '1'
 import mysql.connector
 from model.group import Group
+from model.address import Address
 
 class DbFixture:
 
@@ -19,6 +20,21 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id),name=name,header=header,footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_address_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id,firstname,middlename,lastname,nickname,company,email,email2,email3,mobile,work,home,phone2,address from addressbook")
+            for row in cursor:
+                (id,firstname,middlename,lastname,nickname,company,email,email2,email3,mobile,work,home,phone2,address) = row
+                list.append(Address(id=str(id),name=firstname,middlename=middlename,lastname=lastname,
+                                    nickname=nickname,company=company,email=email,email2=email2,email3=email3,
+                                    mobilephone=mobile,workphone=work,phone=home,secondaryphone=phone2,
+                                    address=address))
         finally:
             cursor.close()
         return list
--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,5 +1,6 @@
 __author__ = '1'
 from model.group import Group
+from model.address import Address
 
 def test_group_list(app, db):
     ui_list = app.group.get_group_list()
@@ -7,3 +8,10 @@ def test_group_list(app, db):
         return Group(id=group.id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+def test_address_list(app, db):
+    ui_list = app.address.get_address_list()
+    def clean(address):
+        return Address(id=address.id, name=address.name.strip(),lastname=address.lastname.strip())
+    db_list = map(clean, db.get_address_list())
+    assert sorted(ui_list, key=Address.id_or_max) == sorted(db_list, key=Address.id_or_max)
