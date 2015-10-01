4cf94603852d34bff05d17c3fb677abcc869ae82
Lilia Sapurina <yoyxoxo@gmail.com>
2015-10-01 20:19:58 +0300

    Добавлен тест на удаление группы BDD

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -2,6 +2,7 @@ __author__ = '1'
 
 from pytest_bdd import given, when, then
 from model.group import Group
+import random
 
 @given('a group list')
 def group_list(db):
@@ -21,3 +22,26 @@ def verify_group_added(db, group_list, new_group):
     new_groups = db.get_group_list()
     old_groups.append(new_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+@given('a non-empty group list')
+def non_empty_group_list(app, db):
+    if len(db.get_group_list()) == 0:
+        app.group.create(new_group)
+    return db.get_group_list()
+
+@given('a random group from the list')
+def random_group(non_empty_group_list):
+    return random.choice(non_empty_group_list)
+
+@when('I delete the group from the list')
+def delete_group(app, random_group):
+    app.group.delete_group_by_id(random_group.id)
+
+@then('the new group list is equal old list without the deleted group')
+def verify_group_deleted(db, non_empty_group_list, random_group):
+    old_groups = non_empty_group_list
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups.remove(random_group)
+    assert old_groups == new_groups
+
